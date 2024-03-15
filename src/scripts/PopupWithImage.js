67a066fd953798efb876006ea2b65c3dc5061cb5
Victor Salcedo <victorsalcedo@MacBook-Pro-de-Victor.local>
2024-03-15 17:21:23 -0500

    Proyecto

---
+++ src/scripts/PopupWithImage.js
@@ -0,0 +1,16 @@
+import Popup from "./Popup.js";
+
+export default class PopupWithImage extends Popup {
+  constructor(popupSelector) {
+    super(popupSelector);
+    this._popupImage = this._popupElement.querySelector(".card__element-img");
+    this._popupTitle = this._popupElement.querySelector(".card__element-title");
+  }
+
+  open(link, name) {
+    this._popupImage.src = link;
+    this._popupImage.alt = name;
+    this._popupTitle.textContent = name;
+    super.open();
+  }
+}
